de1e06e9ba57118bee96662c3916e3d38a673a7b
Akhil Boddu <akhil1234mara@gmail.com>
2019-05-23 14:44:13 +0200

    Updates

---
+++ precache-manifest.ec7a90fa6273593ffa970f90a6f51042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14ab13a92958af2badd95b1b82346915",
+    "url": "/akhilboddu/react-tensorflow/static/media/shape.14ab13a9.svg"
+  },
+  {
+    "revision": "b1b43e04757308d3ee1e",
+    "url": "/akhilboddu/react-tensorflow/static/js/runtime~main.b1b43e04.js"
+  },
+  {
+    "revision": "4e24da5ce77650273d05",
+    "url": "/akhilboddu/react-tensorflow/static/js/main.4e24da5c.chunk.js"
+  },
+  {
+    "revision": "544c9b96a2bf976416dd",
+    "url": "/akhilboddu/react-tensorflow/static/js/1.544c9b96.chunk.js"
+  },
+  {
+    "revision": "cd89c120db3af242e495cab6c19102df",
+    "url": "/akhilboddu/react-tensorflow/index.html"
+  }
+];
